ea7397d292a9f47c225a576e2d2046be55a99eab
navnitkumar-career <navnitkumar.career@gmail.com>
2023-05-24 11:19:49 +0530

    first commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,11 @@
+repositories {
+    mavenLocal()
+    jcenter()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.cognifide.gradle:aem-plugin:14.2.9")
+    implementation("com.neva.gradle:fork-plugin:5.0.5")
+    implementation("net.researchgate:gradle-release:2.6.0")
+}
